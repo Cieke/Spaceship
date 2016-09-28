816764743fbbf8d46d23cf96f29390f3a88d778a
Nancy De Smet <ciek.eu@gmail.com>
2016-09-28 12:04:49 +0200

    shapes and cases

--- SpaceShip/main.swift
+++ SpaceShip/main.swift
@@ -1,34 +1,58 @@
-//  ARC.swift
+// shapes
 //  Created by Tywin Lannister on 28/09/16.
 
 
 import Foundation
-class Person{
-    
-    
-    
-    let name: String
-    init(name: String) {
-        self.name = name
-        print("\(name) is being initialised")
-    }
-    deinit {
-        print("\(name) is being deinitialised")
-    }
+
+
+
+enum Color{
+    case white
+    case black
+    case blue
+    case red
+    case green
+    case yellow
 }
 
 
-var reference1: Person?
-var reference2: Person?
-var reference3: Person?
+
+class Shape{
+    var sides = 1
+    var color : Color = .white
+    
+func countSides(sides : Int){
+    switch sides{
+    case 1:
+    print("this is a Henagon or in layman's term a circle ")
+    case 2:
+    print("this is a Digon")
+    case 3:
+    print("this is a Triangle")
+    case 4:
+    print("this is a Qudrilateral")
+    
+    default:
+        print("default")
+        }
+    }
+
+    
+    
+    
+}
+
+let shape1 = Shape()
+shape1.color = .red
+shape1.countSides(sides: 1)
+
+print("color = \(shape1.color)")
 
 
-reference1 = Person(name: "Luke")
+let shape2 = Shape()
+shape1.color = .green
+shape1.countSides(sides: 2)
+
+print("color = \(shape2.color)")
 
 
-reference2 = reference1
-reference3 = reference1
-
-reference1 = nil
-reference2 = nil
-//reference3 = nil
