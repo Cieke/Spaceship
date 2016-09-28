9de0ea5d11f3e2d08c833f6d62eb05ff844ff3e6
Nancy De Smet <ciek.eu@gmail.com>
2016-09-28 11:07:30 +0200

    memory management

--- SpaceShip/main.swift
+++ SpaceShip/main.swift
@@ -1,56 +1,34 @@
-//  Calculator.swift
+//  ARC.swift
 //  Created by Tywin Lannister on 28/09/16.
 
 
 import Foundation
-class Calculator
-{
-    var accumulator: Double = 0
+class Person{
     
-    func setAccumulator(value:Double)
-    {
-        accumulator = value
-    }
-    func clear()
-    {
-        accumulator = 0
-    }
-    func add(value:Double)
-    {
-        accumulator += value
-    }
-    func subtract(value:Double)
-    {
-        accumulator -= value
-    }
-    func multiply(value:Double)
-    {
-        accumulator *= value
-    }
-    func divide(value:Double)
-    {
-        accumulator /= value
-    }
-   
     
-}
-class Scientific : Calculator
-{
-    func square()
-    {
-        accumulator *= accumulator
+    
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialised")
+    }
+    deinit {
+        print("\(name) is being deinitialised")
     }
 }
 
-let myCalculator = Scientific()
 
-myCalculator.clear()
-myCalculator.setAccumulator(value: 100)
-myCalculator.add(value: 200)
-myCalculator.divide(value: 200)
-myCalculator.subtract(value: 10)
-myCalculator.multiply(value: 5)
-myCalculator.square()
-print("The result is \(myCalculator.accumulator)")
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
 
 
+reference1 = Person(name: "Luke")
+
+
+reference2 = reference1
+reference3 = reference1
+
+reference1 = nil
+reference2 = nil
+//reference3 = nil
