69d1334a6621dc6c204c09463822b4b25e1afdbd
Nancy De Smet <ciek.eu@gmail.com>
2016-09-28 10:38:50 +0200

    calculator

--- SpaceShip/main.swift
+++ SpaceShip/main.swift
@@ -1,107 +1,56 @@
-//  main.swift
-//  SpaceShip
+//  Calculator.swift
 //  Created by Tywin Lannister on 28/09/16.
 
+
 import Foundation
-
-
-struct Point {
-    var x = 0
-    var y = 0
-}
-
-enum Colour{
-    case white
-    case black
-    case red
-    case yellow
-}
-enum Direction {
-    case up
-    case down
-    case left
-    case right
-}
-
-
-class Spaceship{
+class Calculator
+{
+    var accumulator: Double = 0
     
-    var name = ""
-    var damage = 0
-    var colour : Colour = .white
-    var position = Point()
-    func move(direction: Direction){
-        switch direction {
-        case .up:
-            position.y -= 10
-            print("Moved the spaceship up")
-        case.down:
-            position.y += 10
-            print("Moved the spaceship down")
-        case .left:
-            position.x -= 10
-            print("Moved the spaceship left")
-        case .right:
-            position.x += 10
-            print("Moved the spaceship right")
-        }
-    }
-    func fireWeapon ()
+    func setAccumulator(value:Double)
     {
-        print("Firing weapon")
+        accumulator = value
     }
-    func addDamage()
+    func clear()
     {
-        damage += 10
-        print("10 points of damage has been added")
-        if damage >= 50 {
-            print("Spaceship has exploded")
-        }
+        accumulator = 0
     }
-    init() {
-        name = "Enterprise"
-        damage = 0
-        colour = .white
-        position.x = 0
-        position.y = 0
+    func add(value:Double)
+    {
+        accumulator += value
     }
-    init(name:String, damage:Int, colour:Colour, position:Point) {
-        self.name = name
-        self.damage = damage
-        self.colour = colour
-        self.position = position
+    func subtract(value:Double)
+    {
+        accumulator -= value
     }
-
+    func multiply(value:Double)
+    {
+        accumulator *= value
+    }
+    func divide(value:Double)
+    {
+        accumulator /= value
+    }
+   
+    
+}
+class Scientific : Calculator
+{
+    func square()
+    {
+        accumulator *= accumulator
+    }
 }
 
-let spaceship1 = Spaceship()
-spaceship1.name = "Falcon"
-spaceship1.damage = 10
-spaceship1.colour = .red
-spaceship1.position.x = 10
-spaceship1.position.y = 100
+let myCalculator = Scientific()
 
-let spaceship2 = Spaceship()
-
-print("The spaceship \(spaceship1.name) has \(spaceship1.damage) points of damage")
-spaceship1.move(direction: .up)
-spaceship1.move(direction: .down)
-spaceship1.fireWeapon()
-spaceship1.addDamage()
-spaceship1.addDamage()
-spaceship1.addDamage()
-spaceship1.addDamage()
-spaceship1.addDamage()
-spaceship1.addDamage()
-spaceship1.addDamage()
-spaceship1.addDamage()
-
-print("Spaceship2 \(spaceship2.name) has \(spaceship2.damage) points of damage")
-
-let spaceship3 = Spaceship(name:"Eagle" , damage:60 , colour:.yellow , position:Point())
-print("Spaceship3 \(spaceship3.name) has \(spaceship3.damage) points of damage")
-
-
-
+myCalculator.clear()
+myCalculator.setAccumulator(value: 100)
+myCalculator.add(value: 200)
+myCalculator.divide(value: 200)
+myCalculator.subtract(value: 10)
+myCalculator.multiply(value: 5)
+myCalculator.square()
+print("The result is \(myCalculator.accumulator)")
 
 
