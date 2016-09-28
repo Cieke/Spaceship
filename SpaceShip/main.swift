f41444c00e332f568d38abcd2a157dc9a51507c7
Nancy De Smet <ciek.eu@gmail.com>
2016-09-28 09:35:48 +0200

    Initial Commit

---
+++ SpaceShip/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpaceShip
+//
+//  Created by Tywin Lannister on 28/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
